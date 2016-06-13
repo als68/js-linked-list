44981f2bd4ed1ca038fb9b3abb01a7fb5f8c9326
Andy <suhandyl@gmail.com>
2016-06-13 11:43:40 -1000

    Passed 16 tests, onto remove function

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,83 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
 
+//CREATING A LINKED LIST
+var newLinkedList = linkedListGenerator();
+newLinkedList.add('June');
+newLinkedList.add('July');
+newLinkedList.add('August');
+
+function linkedListGenerator(){
+  var head = null;
+  var tail = null;
+
+  var newObj = {
+    getHead: function(){
+      return head;
+    },
+    getTail: function(){
+      return tail;
+    },
+    add: function(n){
+      //takes in any data value and adds a new node to the end of the list
+      newNode = {
+        value: n,
+        next: null
+      };
+
+      if(head === null){
+        head = newNode;
+        tail = newNode;
+      } else {
+        tail.next = newNode;
+        tail = newNode;
+      }
+      return newNode;
+
+    },
+
+    remove: function(n){
+
+      var thisNode = head;
+      var lastNode;
+      for(var i = 0; i <= n; i++){
+
+
+      lastNode = thisNode;
+
+      if(thisNode === null){
+                return false;
+              }
+
+      thisNode = thisNode.next;
+
+        }
+      return lastNode;
+      },
+
+
+    get: function(n){
+      var thisNode = head;
+      var lastNode;
+      for(var i = 0; i <= n; i++){
+
+
+      lastNode = thisNode;
+
+      if(thisNode === null){
+                return false;
+              }
+
+      thisNode = thisNode.next;
+
+        }
+      return lastNode;
+      },
+
+    insert: function(){
+
+    }
+  };
+  return newObj;
 }
